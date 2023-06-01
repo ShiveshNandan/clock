9600a7f765fd1125f81cf032218be17508db4bcc
Shivesh Nandan <kshivesh929@gmail.com>
2023-06-02 00:23:29 +0530

    complete

---
+++ script.js
@@ -0,0 +1,18 @@
+setInterval(() => {
+    // console.log("hello Shivesh!");
+    d = new Date();
+    ddate = d.getDate();
+    // mdate = d.getMonth();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    // console.log(htime , mtime,stime);
+    hr = 30*htime + mtime/2 ;
+    mr = 6*mtime + stime/12;
+    sr = 6*stime;
+
+    hour.style.transform = `rotate(${hr}deg)`;
+    minute.style.transform = `rotate(${mr}deg)`;
+    second.style.transform = `rotate(${sr}deg)`;
+    date.innerText = ddate;
+}, 100);
