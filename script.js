d0a7f1d4c26161dcef6e2e5260174d1f1f29b2cf
Shivesh Nandan <kshivesh929@gmail.com>
2023-06-02 00:41:14 +0530

    complete

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ setInterval(() => {
     d = new Date();
     ddate = d.getDate();
     // mdate = d.getMonth();
+    milli = d.getMilliseconds()
     htime = d.getHours();
     mtime = d.getMinutes();
     stime = d.getSeconds();
     // console.log(htime , mtime,stime);
-    hr = 30*htime + mtime/2 ;
-    mr = 6*mtime + stime/12;
-    sr = 6*stime;
+    hr = 30*htime + mtime/2  ;
+    mr = 6*mtime + stime/12 ;
+    sr = 6*stime + 6*milli/1000;
 
     hour.style.transform = `rotate(${hr}deg)`;
     minute.style.transform = `rotate(${mr}deg)`;
     second.style.transform = `rotate(${sr}deg)`;
     date.innerText = ddate;
-}, 100);
+}, 1);
